a40fcef857062d5c855ba17f77a9d2c4b22e4efb
Amos Samwel <professorprofessor252@gmail.com>
2024-12-10 13:10:36 +0300

    New Commit

---
+++ LearnigJAVA/Learnscanner/Main.java
@@ -0,0 +1,15 @@
+package Learnscanner;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        //Create the Scanner object to read input from the user
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter your First Name:");
+         String firstName = input.next();
+
+       System.out.println(firstName);  
+
+    }
+}
