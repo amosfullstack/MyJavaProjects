32b2347066a2c31034cc6f60b111a8771f39b5aa
Amos Samwel <professorprofessor252@gmail.com>
2024-12-10 12:05:37 +0300

    First commit

---
+++ LearnigJAVA/MainandME/Me.java
@@ -0,0 +1,8 @@
+public class Me {
+    public static void sayName(){
+        System.out.println("Amos Samwel");
+    }
+    public static void sayAge(){
+        System.out.println("21");
+    }
+}
